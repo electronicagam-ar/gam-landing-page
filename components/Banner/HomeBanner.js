761d80d5649c2e423d86e6998a467ffc906262e8
electronicagam-ar <electronicagam@gmail.com>
2026-09-18 18:53:41 -0300

    taller

--- components/Banner/HomeBanner.js
+++ components/Banner/HomeBanner.js
@@ -46,7 +46,7 @@ export const HomeBanner = () => {
                 <MotionBTTContainer transition={{ delay: 0.8, duration: 0.5 }}>
                     <div className="page-banner--image">
                        <Image
-  src="/hero1.jpg" // CAMBIA .jpg por .png si tu foto nueva es .png
+  src="/taller.jpg" // CAMBIA .jpg por .png si tu foto nueva es .png
   width={1024}
   height={680}
   alt="Taller de Electrónica GAM"

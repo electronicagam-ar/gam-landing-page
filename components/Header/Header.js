2a017d76f2c322b9d88707873360f9080522d256
electronicagam-ar <electronicagam@gmail.com>
2026-09-23 12:14:04 -0300

    gam

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -13,7 +13,7 @@ export const Header = () => {
           <h1 className="logo mb-0">
             <Link href="/">
               <Image
-                src="/logo_GAM.jpeg"
+                src="/logo-gam.jpeg"
                 alt="logo"
                 className="h-16 w-auto object-contain"
                 height={100}

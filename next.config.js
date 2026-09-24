9bbd99d3e49c101633a666cf718bda999f5debb3
electronicagam-ar <electronicagam@gmail.com>
2026-09-24 20:42:19 -0300

    liii

--- next.config.js
+++ next.config.js
@@ -21,7 +21,7 @@ const nextConfig = {
         siteDescription: "Reparación de Smart TVs LED, LCD, OLED, QLED y 4K en Santa Fe Capital. Más de 25 años de experiencia. Presupuesto sin cargo.",
         siteKeywords: "reparación de tv santa fe, service smart tv santa fe, reparación led oled qled, electrónica gam",
         siteUrl: "https://www.electronicagam.com.ar",
-        siteImagePreviewUrl: "/images/preview.jpeg",
+        siteImagePreviewUrl: "images/preview.jpeg",
         twitterHandle: "@electronicagam"
     },
     headers() {
